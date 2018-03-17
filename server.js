2a1ca0e39fc3dd59142220028f5071a5f23460de
Kevin Olds <kolds@themeetgroup.com>
2018-03-17 13:24:21 -0400

    KRO - http module server

--- server.js
+++ server.js
@@ -1,4 +1,33 @@
-import config , { nodeEnv, logStars } from './config';
+import http from 'http';
+import config from './config';
 
-console.info(config, nodeEnv);
-console.info(logStars('we can have the function run, then return a value also'));
+// Core module https way
+/*
+https.get('https://www.lynda.com', (res) => {
+  const chunksArr = [];
+  console.info('Response Status Code: ', res.statusCode);
+
+  res.on('data', (chunk) => {
+    chunksArr.push(chunk.toString());
+  });
+
+  res.on('end', () => {
+    console.info(chunksArr.join());
+  })
+
+});
+//*/
+
+// Express way
+const server = http.createServer();
+
+server.listen(config.port);
+console.info(`server listenting on port: ${config.port}`);
+
+server.on('request', (req, res) => {
+  res.write('Hello HTTP\n');
+  setTimeout(() => {
+    res.write('I can stream!\n');
+    res.end();
+  }, 3000);
+});
